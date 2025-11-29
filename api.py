a178bdcfd3b16d7d0248ef64d6c69fe5859a6b4c
Omkar Kanase <129031542+Omkar8284@users.noreply.github.com>
2025-11-29 23:19:42 +0530

    Update api.py

--- api.py
+++ api.py
@@ -2,7 +2,6 @@ from fastapi import FastAPI
 import joblib
 import numpy as np
 
-# Load the saved model
 model = joblib.load("house_price_model.pkl")
 
 app = FastAPI()
@@ -13,7 +12,6 @@ def root():
 
 @app.get("/predict")
 def predict(area: float, bedrooms: int):
-    # Predict using the loaded model
     input_features = np.array([[area, bedrooms]])
     price = model.predict(input_features)[0]
     return {"predicted_price": float(price)}
